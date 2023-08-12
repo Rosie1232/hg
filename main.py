373cc6d335f53779a82f195b69b45b14352c70c6
Rosie1232 <142110839+Rosie1232@users.noreply.github.com>
2023-08-12 18:34:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-
+meme_dict = {
+             "CRINGE": "garip ya da utandırıcı bir davranış",
+             "LOL" : "komik bir duruma verilen tepki (açılımı: Lots Of Laugh)",
+             "IDK" : "bilmiyorum kelimesinin ingilizce karşılığı (I don't know)",
+             "BRUH" : "rezil, utanç verici bir durum anında kullanılır",
+             "DM" : "sosyal medya uygulamalarında mesaj anlamındadır"
+}
+for i in range(5):
+    word = input ("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
+    if word in meme_dict.keys():
+        print(meme_dict[word])
+    else:
+        print("lütfen kurallara uygun bir kelime yazdığınızdan emin olun")
