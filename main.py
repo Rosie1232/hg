b553f69e69a60c91aebed530df2f2ca7a6ef9e5b
Rosie1232 <142110839+Rosie1232@users.noreply.github.com>
2023-08-19 18:51:53 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-meme_dict = {
-             "CRINGE": "garip ya da utandırıcı bir davranış",
-             "LOL" : "komik bir duruma verilen tepki (açılımı: Lots Of Laugh)",
-             "IDK" : "bilmiyorum kelimesinin ingilizce karşılığı (I don't know)",
-             "BRUH" : "rezil, utanç verici bir durum anında kullanılır",
-             "DM" : "sosyal medya uygulamalarında mesaj anlamındadır"
-}
-for i in range(5):
-    word = input ("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
-    if word in meme_dict.keys():
-        print(meme_dict[word])
-    else:
-        print("lütfen kurallara uygun bir kelime yazdığınızdan emin olun")
+import random
+
+
+krktrler = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+parola = ""
+
+
+uzun = int(input("parola uzunluğunu giriniz:"))
+for i in range(0, uzun):
+    parola[len] == uzun
+
+parola = random.choice(krktrler)
+print(parola)
